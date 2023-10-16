0eda4d6005d2dbee2751b3b9b29fab8745b4b41c
Luis Zubia <soyzubia@gmail.com>
2023-10-16 11:29:11 -0700

    Setting initial routes according to the ones showed in the wireframe images

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,26 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # get "up" => "rails/health#show", as: :rails_health_check
+
+  id_pattern = /\d+/
+  # id must match a number for at least one digit
+  constraints(id: id_pattern, user_id: id_pattern, recipe_id: id_pattern, food_id: id_pattern, recipe_food_id: id_pattern) do
+    get 'public_recipes', to: 'public_recipes#index'
+    get 'generate_shopping_list', to: 'shopping_list#index'
+
+    resources :public_recipes, only: %i[index show]
+    resources :recipe_foods, only: %i[update destroy]
+
+    resources :users, only: %i[new create] do
+      resources :shopping_list, only: %i[index]
+    end
+
+    resources :foods, only: %i[index show new create destroy]
+    resources :recipes, only: %i[index show new create destroy] do
+      resources :recipe_foods, only: %i[index new create update]
+    end
+  end
 
   # Defines the root path route ("/")
   # root "posts#index"
