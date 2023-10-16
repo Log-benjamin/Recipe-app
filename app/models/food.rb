008618370a8db3d6e4686db36c3e9a1bcf92be6c
Luis Zubia <soyzubia@gmail.com>
2023-10-16 09:06:44 -0700

    Fix name length validation

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,9 @@
 class Food < ApplicationRecord
-  validates :name, presence: { message: 'must be provided' }, length: { minimum: 3, maximum: 50 }
+  validates :name, presence: { message: 'must be provided' }
+  validates :name, length: { minimum: 3, maximum: 65, message: "at least 3 characters and can't exceed 65 characters" }
   validates :measurement_unit, presence: { message: 'must be provided' }
+  validates :measurement_unit,
+            length: { minimum: 3, maximum: 30, message: "at least 3 characters and can't exceed 30 characters" }
   validates :price, presence: { message: "can't be zero" }, numericality: { greater_than: 0 }
   validates :quantity, presence: { message: "can't be zero" }, numericality: { greater_than: 0 }
 
