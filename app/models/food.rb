62ae4c4034ca92934990e372d9106640d8a1fadc
binyamT <binyampowerhc@gmail.com>
2023-10-17 14:13:04 +0300

    add validation for name, measurment, and price of Model Food

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,8 @@
 class Food < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :measurement_unit, presence: true
+  validates :price, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
   belongs_to :user
   has_many :recipe_foods
   has_many :recipes, through: :recipe_foods
