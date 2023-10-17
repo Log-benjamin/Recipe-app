546b93fa3b7353e2820eacd8df9b145792e69013
binyamT <binyampowerhc@gmail.com>
2023-10-17 14:14:42 +0300

    add validation for name, preparation time, cooking time, and description of Model Recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,9 @@
 class Recipe < ApplicationRecord
+  validates :name, presence: true
+  validates :preparation_time, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :cooking_time, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :description, presence: true
+
   belongs_to :user
   has_many :recipe_foods
   has_many :foods, through: :recipe_foods
