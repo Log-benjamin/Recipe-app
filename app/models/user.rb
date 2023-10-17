4fe9390ee690f1b17683a9979a15901b642b40d9
binyamT <binyampowerhc@gmail.com>
2023-10-17 23:17:58 +0300

    add and install Devise gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
   validates :name, presence: { message: 'must be provided' }
   validates :name, length: { maximum: 65, message: "can't exceed 65 characters" }
 
