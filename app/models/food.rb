1846d5361ec6555f4cc0a5355c04a9640be5004f
Luis Zubia <soyzubia@gmail.com>
2023-10-18 07:58:03 -0700

    Fix models validations

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,16 +1,24 @@
 class Food < ApplicationRecord
   validates :name, :measurement_unit, :price, :quantity, presence: { message: 'must be provided' }
-  validates :name, format: { with: /\A[a-z ,.'-]+\z/i, message: 'provide a valid `%<attribute>`' }
-  validates :name, length: { minimum: 3, too_short: 'must be at least `%<count>` characters' }
-  validates :name, length: { maximum: 65, too_long: "can't exceed `%<count>` characters" }
-  validates :name, uniqueness: { scope: :user, message: '`%<attribute>` was added previously' }
-  validates :measurement_unit, length: { minimum: 2, too_short: 'must be at least `%<count>` characters' }
-  validates :measurement_unit, length: { maximum: 30, too_long: "can't exceed `%<count>` characters" }
-  validates :measurement_unit, uniqueness: { message: '`%<attribute>` already exists' }
+  validates :name, format: { with: /\A[a-z ,.'-]+\z/i,
+                             message: 'provided `%<value>s` is not a valid' },
+                   allow_nil: false, allow_blank: false
+  validates :name, length: { minimum: 3, too_short: 'must be at least %<count>s characters' }
+  validates :name, length: { maximum: 65, too_long: "can't exceed %<count>s characters" }
+  validates :name, uniqueness: { scope: :user, case_sensitive: false,
+                                 message: 'provided `%<value>s` seems that it already exists' }
+  validates :measurement_unit, length: { minimum: 2, too_short: 'must be at least %<count>s characters' }
+  validates :measurement_unit, length: { maximum: 30, too_long: "can't exceed %<count>s characters" }
+  validates :measurement_unit, uniqueness: { scope: %i[user name],
+                                             message: 'provided `%<value>s` seems that it already exists' }
   validates :price, numericality: { greater_than: 0, message: 'must be greater than zero' }
   validates :quantity, numericality: { greater_than: 0, message: 'must be greater than zero' }
 
   belongs_to :user
-  has_many :recipe_foods, inverse_of: :food
+  has_many :recipe_foods, inverse_of: :food, dependent: :destroy
   has_many :recipes, through: :recipe_foods
+
+  def to_s
+    name
+  end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,8 +1,9 @@
 class Recipe < ApplicationRecord
   validates :name, :preparation_time, :cooking_time, :description, presence: { message: 'must be provided' }
-  validates :name, format: { with: /\A[a-z ,.'-]+\z/i, message: 'provide a valid `%<attribute>`' }
-  validates :name, length: { minimum: 3, too_short: 'must be at least `%<count>` characters' }
-  validates :name, length: { maximum: 150, too_long: "can't excee `%<count>` characters" }
+  validates :name, format: { with: /\A[a-z ,.'-]+\z/i, message: 'provided `%<value>s` is not a valid' },
+                   allow_nil: false, allow_blank: false
+  validates :name, length: { minimum: 3, too_short: 'must be at least %<count>s characters' }
+  validates :name, length: { maximum: 150, too_long: "can't exceed %<count>s characters" }
   validates_each :preparation_time, :cooking_time do |record, attr, value|
     record.errors.add(attr, 'must be time') unless value.acts_like?(:time)
     record.errors.add(attr, 'must be greater than 0 minutes') unless value.min.positive? || value.hour.positive?
