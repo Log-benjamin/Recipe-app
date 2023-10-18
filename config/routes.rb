a36472a50f01d041eed7902235c2e0cfc779d2cc
Luis Zubia <soyzubia@gmail.com>
2023-10-18 09:15:57 -0700

    Adjust routes to use the public_recipes path

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
                      path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret',
                                    confirmation: 'verification', unlock: 'unblock', registration: 'register',
                                   sign_up: 'join' }
-  
+
   root to: 'recipes#index'
 
   devise_scope :user do
@@ -13,10 +13,9 @@ Rails.application.routes.draw do
   id_pattern = /\d+/
   # id must match a number for at least one digit
   constraints(id: id_pattern, user_id: id_pattern, recipe_id: id_pattern, food_id: id_pattern, recipe_food_id: id_pattern) do
-    get 'public_recipes', to: 'public_recipes#index'
     get 'generate_shopping_list', to: 'shopping_list#index'
 
-    resources :public_recipes, only: %i[index show]
+    resources :public_recipes, only: %i[index]
     resources :recipe_foods, only: %i[update destroy]
 
     resources :users, only: %i[new create] do
