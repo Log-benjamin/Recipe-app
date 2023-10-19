482f259189a1ced0aa9c458bba2f9fabe2fa3f79
Luis Zubia <soyzubia@gmail.com>
2023-10-18 17:42:55 -0700

    Allow all users to read a Recipe

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,7 @@ class Ability
   def initialize(current_user)
     user = current_user || User.new
 
-    can :read, Recipe, user_id: user.id
+    can :read, Recipe
     can :update, Recipe, user_id: user.id
     can :destroy, Recipe, user_id: user.id
   end
