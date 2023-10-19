05d32b41ff639e8e386cddf38d0f2ace10d2ff68
Luis Zubia <soyzubia@gmail.com>
2023-10-18 17:34:58 -0700

    Reconfigure the routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,22 +13,18 @@ Rails.application.routes.draw do
   id_pattern = /\d+/
   # id must match a number for at least one digit
   constraints(id: id_pattern, user_id: id_pattern, recipe_id: id_pattern, food_id: id_pattern, recipe_food_id: id_pattern) do
-    get 'generate_shopping_list', to: 'shopping_list#index'
-
     resources :public_recipes, only: %i[index]
-    resources :recipe_foods, only: %i[update destroy]
+    resources :recipe_foods, only: %i[new create edit update destroy]
+    get 'general_shopping_list', to: 'shopping_list#index'
 
-    resources :users, only: %i[new create] do
-      resources :shopping_list, only: %i[index]
-    end
+    resources :users, only: %i[new create]
 
-    resources :foods, only: %i[index show new create destroy]
+    resources :foods, only: %i[index new create destroy]
     resources :recipes, only: %i[index show new create destroy] do
       member do
         patch 'update_privacy', to: 'recipes#updatePrivacy', as: 'update_recipe_privacy'
       end
     end
-    resources :recipe_foods, only: %i[new create update]
   end
 
 end
