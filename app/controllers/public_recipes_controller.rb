f9ff2fdf7bef27719f4d4b95bd8d4a539baf802d
Luis Zubia <soyzubia@gmail.com>
2023-10-18 17:36:23 -0700

    Add PublicRecipes controller

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,4 +1,7 @@
 class PublicRecipesController < ApplicationController
+  skip_before_action :authenticate_user!
+
   def index
+    @public_recipes = Recipe.is_public.order(created_at: :desc)
   end
 end
