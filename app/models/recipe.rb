2943dbec30f35af1f5877b3c45d8ae4e6318cb2e
binyamT <binyampowerhc@gmail.com>
2023-10-20 19:34:40 +0300

    update the recipe constraint and schema

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,14 +4,12 @@ class Recipe < ApplicationRecord
                    allow_nil: false, allow_blank: false
   validates :name, length: { minimum: 3, too_short: 'must be at least %<count>s characters' }
   validates :name, length: { maximum: 150, too_long: "can't exceed %<count>s characters" }
-  validates_each :preparation_time, :cooking_time do |record, attr, value|
-    record.errors.add(attr, 'must be time') unless value.acts_like?(:time)
-    record.errors.add(attr, 'must be greater than 0 minutes') unless value.min.positive? || value.hour.positive?
-    record.errors.add(attr, 'must be less than 24 hours') unless value.hour < 24
-  end
+
+  validates :preparation_time, numericality: { greater_than: 0, message: 'must be greater than zero' }
+  validates :cooking_time, numericality: { greater_than: 0, message: 'must be greater than zero' }
 
   belongs_to :user
-  has_many :recipe_foods, inverse_of: :recipe
+  has_many :recipe_foods, dependent: :destroy
   has_many :foods, through: :recipe_foods
 
   scope :is_public, -> { where(public: true) }
