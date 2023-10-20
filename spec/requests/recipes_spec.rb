2b172ad47cdd7e3e8686a7ae624205974524d5a1
Luis Zubia <soyzubia@gmail.com>
2023-10-20 16:48:27 -0700

    Add test for 'recipes#show' requests

--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "'Recipes' - [Controller]", :requests, type: :request do
   let!(:user) { User.create(name: 'test', email: 'test@me.com', password: 'test1234', confirmed_at: Time.now) }
+  let!(:recipe) { Recipe.create(user:, name: 'Test recipe', description: 'Test description recipe', preparation_time: 1, cooking_time: 1.5) }
 
   describe "'GET /recipes' => 'index' action at 'recipes' controller", :index do
     describe "* 'status'" do
@@ -45,6 +46,21 @@ RSpec.describe "'Recipes' - [Controller]", :requests, type: :request do
 
       it "- 'body' includes 'Recipe page'" do
         expect(response.body).to match(/Recipe Page/)
+      end
+    end
+  end
+
+  describe "'GET /show' => 'show' action at 'recipes' controller", :show do
+    before do
+      sign_in user
+      user.recipes.create(name: 'Test recipe', description: 'Test description recipe', preparation_time: 1, cooking_time: 1.5)
+      get recipe_path(user.recipes.last)
+    end
+
+    context "* 'status'", :status do
+      it '- returns http success/ok' do
+        expect(response).to have_http_status(:success)
+        expect(response).to have_http_status(:ok)
       end
     end
   end
